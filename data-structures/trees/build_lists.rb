5dcff78fc1d5feb33e14492e72d50ceb3e9357d4
Juan Pemberthy <jpemberthy@gmail.com>
2014-09-28 17:26:14 -0700

    build lists from tree.

---
+++ data-structures/trees/build_lists.rb
@@ -0,0 +1,24 @@
+# Given a binary search tree, design an algorithm which creates a linked list of all the nodes at each depth
+# (i e , if you have a tree with depth D, you’ll have D linked lists)
+
+
+@tree = Hash.new { |hsh, k| hsh[k] = Array.new }
+@tree[0] = [1] # root
+@tree[1] = [2, 3]
+@tree[3] = [4, 5]
+@tree[4] = [10, 15]
+@tree[5] = [20, 30]
+
+@lists = Array.new
+
+def build_lists(node, li=0)
+  @lists[li] ||= Array.new
+  @tree[node].each do |x|
+    @lists[li] << x
+    build_lists(x, li+1)
+  end
+end
+
+build_lists 0
+
+p @lists
