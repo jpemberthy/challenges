d414a159639b3da9a200d676452212beeb2615db
Juan Pemberthy <jpemberthy@gmail.com>
2014-10-08 22:44:16 -0700

    euler/problem_2

---
+++ euler/problem_2.rb
@@ -0,0 +1,23 @@
+# https://projecteuler.net/problem=2
+
+@cache = {}
+@cache[0] = 0
+@cache[1] = 1
+@cache[2] = 2
+
+def fib(n)
+  return @cache[n] if @cache[n]
+  return n if n <= 2
+  @cache[n] = fib(n-1) + fib(n-2)
+end
+
+n = 4_000_000
+sum = val = i = 0
+
+while val < n
+  val = fib(i)
+  sum += val if val % 2 == 0 && val < n
+  i += 1
+end
+
+p sum
