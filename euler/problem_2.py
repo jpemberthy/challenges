0239ab4af8910bf51ee01d9c4ad6415f0b86ca05
Juan Pemberthy <jpemberthy@gmail.com>
2014-10-13 21:52:05 -0700

    Here we go!
    * God I miss n = 4_000_000
    * why the fuck sum is a built in function!

---
+++ euler/problem_2.py
@@ -0,0 +1,23 @@
+# https://projecteuler.net/problem=2
+
+cache = {}
+cache[0] = 0
+cache[1] = 1
+cache[2] = 2
+
+def fib(n):
+    if n in cache: return cache[n]
+    cache[n] = fib(n-1) + fib(n-2)
+    return cache[n]
+
+n = 4000000
+# there's a sum built in function.
+_sum = val = i = 0
+
+while val < n:
+    val = fib(i)
+    if val % 2 == 0 and val < n:
+        _sum += val
+    i += 1
+
+print(_sum)
