eb43527959eceece3f258a4dd71b77b5f1679014
Juan Pemberthy <juanp@uber.com>
2018-01-22 20:57:11 -0800

    Add two_sum warm up

---
+++ warm-up/two_sum.go
@@ -0,0 +1,30 @@
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// Example:
+// Given nums = [2, 7, 11, 15], target = 9,
+
+// Because nums[0] + nums[1] = 2 + 7 = 9,
+// return [0, 1].
+
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
+
+func main() {
+	// fmt.Println(twoSum([]int{1, 3, 5, 100}, 105))
+	fmt.Println(twoSum([]int{3, 2, 4}, 6))
+}
