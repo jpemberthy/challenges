33bdf95e44a4984fda273d622619025d624778a5
Juan Pemberthy <juanp@uber.com>
2018-09-10 23:23:10 -0700

    Fix main

--- Leetcode/23_Merge_k_Sorted_Lists.go
+++ Leetcode/23_Merge_k_Sorted_Lists.go
@@ -68,5 +68,5 @@ func main() {
 		&ListNode{Val: 1, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4}}},
 		&ListNode{Val: 2, Next: &ListNode{Val: 6}},
 	}
-	mergeKListsShort(emptyList)
+	mergeKLists(lists)
 }
